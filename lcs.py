37ddcc1b900e65dd886a9359da27b54a1e984632
Eitan Romanoff <ear7631@gmail.com>
2012-10-10 18:19:42 -0400

    Algorithm B in linear space quadratic time is implemented   - That paper has the worst notation (and mistakes)

--- lcs.py
+++ lcs.py
@@ -34,5 +34,44 @@ def dynamicGetLCS(first, second):
 
     TODO: Function currently returns the matrix. Use the matrix to get the LCS.
     """
+    """
+    start at the very end of the dual array
+        if the result_matrix[i][j] == result_matrix[i][j-1]
+            move up and make recursive call
+        if the result_matrix[i][j] == result_matrix[i-1][j]
+            move left and make the recursive call
+        else
+            move diag and concat first[i]
+    """
     result_matrix = dynamicGenerateMatrix(first, second)
     return result_matrix
+
+
+def algorithmB(first, second):
+    k = [[0] * (len(second) + 1), [0] * (len(second) + 1)]
+    for i in range(1, len(first) + 1):
+        temp = k[0]
+        k[0] = k[1]
+        k[1] = temp
+        for j in range(1, len(second) + 1):
+            if first[i - 1] == second[j - 1]:
+                k[1][j] = k[0][j - 1] + 1
+            else:
+                k[1][j] = max(k[1][j - 1], k[0][j])
+    return k[1]
+
+
+def algorithmc(first, second):
+    m = len(first)
+    n = len(second)
+    C = ""
+    # if problem is trivial, solve it
+    if n == 0:
+        return C
+    else:
+        i = m // 2
+    pass
+
+
+def algorithmb(first, second):
+    pass
